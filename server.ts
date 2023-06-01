bc77bd56667ee1bf4212d58745ca248575be9271
unknown <diptodey2005@gmail.com>
2023-06-01 23:25:14 +0600

    till ts config added

---
+++ server.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/test');
+        console.log("Database is connected successfully");
+        
+    } catch (error) {
+        
+    }
+    
+  }
