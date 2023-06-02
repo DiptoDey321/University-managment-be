7e44ee74fb1fb2e50a5574a52713dc12fff51d19
unknown <diptodey2005@gmail.com>
2023-06-02 10:30:57 +0600

    database connecction setup is done

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+// import
+import app from "./app";
+import config from "./config/index";
+// import appp
+
+async function main() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log("Database is connected successfully");
+
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+        })
+        
+    } catch (error) {
+        console.log("Failed to connet",error);
+    }
+  }
+
+main();
