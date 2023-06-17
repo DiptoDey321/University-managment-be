56c59776feb60cd0b75b1362d39bb817dae26daf
Dipto Dey <diptode2005@gmail.com>
2023-06-17 14:06:47 +0600

    checking

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-const app: Application = express()
+// import express, { Application, Request, Response } from 'express'
+// import cors from 'cors'
+// const app: Application = express()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
-// test
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working successfully!')
-})
+// // test
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Working successfully!')
+// })
 
-export default app
+// export default app
